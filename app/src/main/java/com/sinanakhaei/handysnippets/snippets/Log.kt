b96a5a36a5a77c06b100108cd7450128be42a3cc
Sina <cna.nakhaei@gmail.com>
2024-01-23 12:06:18 +0330

    feat: implement Log snippets

---
+++ app/src/main/java/com/sinanakhaei/handysnippets/snippets/Log.kt
@@ -0,0 +1,52 @@
+package com.sinanakhaei.handysnippets.snippets
+
+import android.util.Log
+
+fun Any.logMe(tag: String = this.tag) {
+    d(tag, this.toString())
+}
+
+fun Any.v(message: () -> String) {
+    if (Log.isLoggable(tag, Log.VERBOSE)) v(message())
+}
+
+fun Any.d(message: () -> String) {
+    if (Log.isLoggable(tag, Log.DEBUG)) d(message())
+}
+
+fun Any.i(message: () -> String) {
+    if (Log.isLoggable(tag, Log.INFO)) i(message())
+}
+
+fun Any.e(message: () -> String) {
+    if (Log.isLoggable(tag, Log.ERROR)) e(message())
+}
+
+fun Any.wtf(message: () -> String) = w(message())
+
+fun Any.v(message: String) = v(tag, message)
+
+fun Any.d(message: String) = d(tag, message)
+
+fun Any.i(message: String) = i(tag, message)
+
+fun Any.w(message: String) = w(tag, message)
+
+fun Any.e(message: String) = e(tag, message)
+
+fun Any.wtf(message: String) = wtf(tag, message)
+
+inline fun v(tag: String, message: String) = Log.v(tag, message)
+
+inline fun d(tag: String, message: String) = Log.d(tag, message)
+
+inline fun i(tag: String, message: String) = Log.i(tag, message)
+
+inline fun w(tag: String, message: String) = Log.w(tag, message)
+
+inline fun e(tag: String, message: String) = Log.e(tag, message)
+
+inline fun wtf(tag: String, message: String) = Log.wtf(tag, message)
+
+private val Any.tag: String
+    get() = javaClass.simpleName
