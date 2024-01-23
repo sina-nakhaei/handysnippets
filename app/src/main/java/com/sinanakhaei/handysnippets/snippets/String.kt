7dd2f703b27a106887341b3a59a2326b92c5e6fa
Sina <cna.nakhaei@gmail.com>
2024-01-23 12:44:52 +0330

    update<String>: implement truncate method

--- app/src/main/java/com/sinanakhaei/handysnippets/snippets/String.kt
+++ app/src/main/java/com/sinanakhaei/handysnippets/snippets/String.kt
@@ -6,3 +6,10 @@ inline fun String.isEmail(): Boolean {
     val emailRegex = Regex("[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}")
     return emailRegex.matches(this)
 }
+
+fun String.truncate(maxLength: Int): String {
+    if (length <= maxLength) {
+        return this
+    }
+    return substring(0, maxLength).trimEnd() + "..."
+}
