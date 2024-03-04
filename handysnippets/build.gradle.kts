1e7eb0881b160889fb9fe192765a9824b71fb402
esfandiary <abbas.ha.1378@gmail.com>
2024-03-04 08:36:50 +0330

    fix build error

--- handysnippets/build.gradle.kts
+++ handysnippets/build.gradle.kts
@@ -48,6 +48,13 @@ android {
         jvmTarget = "1.8"
     }
 
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.3"
+    }
+
     publishing {
         multipleVariants {
             allVariants()
@@ -62,8 +69,13 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.8.0")
+    implementation("androidx.compose.foundation:foundation:1.6.1")
+    implementation("androidx.compose.ui:ui:1.6.1")
+    implementation("androidx.compose.ui:ui-graphics:1.6.1")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.6.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 }
 
