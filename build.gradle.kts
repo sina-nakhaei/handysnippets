81798a920b146f38e5e43e1884278dc142e9beb0
esfandiary <abbas.ha.1378@gmail.com>
2024-03-04 09:03:45 +0330

    fix build error

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,6 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("com.android.library") version "8.2.0" apply false
 }
+tasks.register("clean", Delete::class) {
+    delete(layout.buildDirectory)
+}
